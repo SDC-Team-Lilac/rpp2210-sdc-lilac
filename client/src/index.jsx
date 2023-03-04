a85ea5d22b54fe41883c5fdcf85ea6290ae60752
George Liu <georgeliu8110@gmail.com>
2023-03-04 14:33:28 -0600

    add html and App.jsx to render

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,15 @@
-//
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+const App = () => {
+  return (
+  <div>
+    Hello World!
+  </div>
+  )
+};
+
+root.render(<App />);
