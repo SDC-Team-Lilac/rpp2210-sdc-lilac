4ca921c17a38ccf0f6a22c0a53f7b177976d6448
Rachel Quirbach <rachelquirbachy@gmail.com>
2023-03-04 16:34:59 -0500

    "Commit progress at end of Saturday group time - added server/api_handler, running into bug with receiving axios request"

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const api = require('./api_handler.js');
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname,'../client/dist')));
 
+app.get('/products', (req, res) => {
+  api.getAllProducts()
+    .then(allProducts => {
+      console.log('All Products: ', allProducts);
+      res.status(200).send(allProducts);
+    })
+    .catch(error => {
+      console.error('Error in getAllProducts: ', error);
+      res.status(404).send(error);
+    })
+})
+
 app.listen(3000, function(){
   console.log('connected to server!')
-})
+});
