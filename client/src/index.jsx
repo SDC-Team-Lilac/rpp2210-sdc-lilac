74faf5e6ea7f97dfbe7b011d554ab4c52872d1b0
Christian Willsea <christianwillsea@gmail.com>
2023-03-06 18:26:36 -0800

    Render each component in app component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import Overview from './components/overview/Overview.jsx'
+import QA from './components/qa/QA.jsx'
+import RelatedProducts from './components/relatedProducts/RelatedProducts.jsx'
+import Reviews from './components/reviews/Reviews.jsx'
+
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
@@ -8,6 +13,10 @@ const App = () => {
   return (
   <div>
     Hello World!
+    <Overview />
+    <RelatedProducts />
+    <QA />
+    <Reviews />
   </div>
   )
 };
