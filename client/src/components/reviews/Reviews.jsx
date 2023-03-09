f8ddd9a1ba50a20ed7ab4964b90437b38f78a45b
Christian Willsea <christianwillsea@gmail.com>
2023-03-08 21:42:36 -0800

    Add subcomponents to main Reviews component

--- client/src/components/reviews/Reviews.jsx
+++ client/src/components/reviews/Reviews.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import RatingBreakdown from './RatingBreakdown.jsx'
+import ReviewTile from './ReviewTile.jsx'
+import SortOptions from './SortOptions.jsx'
+import ReviewList from './ReviewList.jsx'
+import NewReview from './NewReview.jsx'
+import KeywordSearch from './KeywordSearch.jsx'
 
 const Reviews = () => {
 
   return (
-    <div> Reviews! </div>
+    <div>
+      <h1>Reviews!</h1>
+      <RatingBreakdown />
+      <ReviewList/>
+    </div>
   )
 }
 
