d2a257de7df76782141a6368c4992158e4e405c3
Christian Willsea <christianwillsea@gmail.com>
2023-03-10 23:10:49 -0800

    Add initial planning, state & wireframes for each component

--- client/src/components/reviews/ReviewTile.jsx
+++ client/src/components/reviews/ReviewTile.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const ReviewTile = () => {
+const ReviewTile = ({ review }) => {
 
+  /* This component:
+    1) needs an individual review
+    2) Will render data for: star rating, date of review, review summary, review body, recommend, reviewer name, response to review, rating helpfulness
+   */
   return (
-    <div> ReviewTile! </div>
+    <div style={{border: '2px solid purple'}}> ReviewTile! </div>
   )
 }
 
