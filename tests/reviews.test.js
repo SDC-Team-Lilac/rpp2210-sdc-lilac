aa722a57b2eb32bb5f0b8c356b01703d73b8d88a
Christian Willsea <christianwillsea@gmail.com>
2023-03-16 22:00:12 -0700

    Add prelimary tests for review components

--- tests/reviews.test.js
+++ tests/reviews.test.js
@@ -1,14 +1,75 @@
 import * as React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 // import ReactDOM from 'react-dom';
 // import App from '../client/src/index.jsx'
+import Characteristics from '../client/src/components/reviews/Characteristics.jsx';
+import KeywordSearch from '../client/src/components/reviews/KeywordSearch.jsx';
+import NewReview from '../client/src/components/reviews/NewReview.jsx';
+import ProductBreakdown from '../client/src/components/reviews/ProductBreakdown.jsx';
+import RatingBreakdown from '../client/src/components/reviews/NewReview.jsx';
+import ReviewList from '../client/src/components/reviews/ReviewList.jsx';
 import Reviews from '../client/src/components/reviews/Reviews.jsx';
+import ReviewTile from '../client/src/components/reviews/ReviewTile.jsx';
+import SortOptions from '../client/src/components/reviews/SortOptions.jsx';
 
 // jest.mock('react-dom', () => ({ render: jest.fn() }))
 
-describe('Reviews Component', () => {
-  it('Renders Reviews component',  () => {
-    render(<Reviews />)
+xdescribe('Characteristics Component', () => {
+  it('Renders Characteristics component',  () => {
+    render(<Characteristics />)
     screen.debug()
   })
+})
+xdescribe('KeyWordSearch Component', () => {
+    it('Renders KeyWordSearch component',  () => {
+      render(<KeyWordSearch />)
+      screen.debug()
+    })
+  })
+xdescribe('NewReview Component', () => {
+    it('Renders NewReview component',  () => {
+    render(<NewReview />)
+    screen.debug()
+    })
+})
+
+describe('ProductBreakdown Component', () => {
+    it('Renders ProductBreakdown component',  () => {
+        render(<ProductBreakdown />)
+        screen.debug()
+    })
+})
+
+xdescribe('RatingBreakdown Component', () => {
+    it('Renders RatingBreakdown component',  () => {
+        render(<RatingBreakdown />)
+        screen.debug()
+    })
+})
+
+xdescribe('ReviewList Component', () => {
+    it('Renders ReviewList component',  () => {
+        render(<ReviewList />)
+        screen.debug()
+    })
+})
+
+describe('Reviews Component', () => {
+    it('Renders Reviews component', () => {
+        render (<Reviews productId={71697}/>)
+    })
+})
+
+xdescribe('ReviewTile Component', () => {
+    it('Renders ReviewTile component',  () => {
+        render(<ReviewTile />)
+        screen.debug()
+    })
+})
+
+xdescribe('SortOptions Component', () => {
+    it('Renders SortOptions component',  () => {
+        render(<SortOptions />)
+        screen.debug()
+    })
 })
