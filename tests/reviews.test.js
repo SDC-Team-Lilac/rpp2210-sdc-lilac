27d4a64b5ec7acc9eaec7f4f2e2b52a82a78aea6
Christian Willsea <christianwillsea@gmail.com>
2023-03-16 20:37:22 -0700

    Add testing for reviews.test.js

--- tests/reviews.test.js
+++ tests/reviews.test.js
@@ -1,9 +1,14 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+// import ReactDOM from 'react-dom';
+// import App from '../client/src/index.jsx'
 import Reviews from '../client/src/components/reviews/Reviews.jsx';
 
+// jest.mock('react-dom', () => ({ render: jest.fn() }))
+
 describe('Reviews Component', () => {
-  it('Renders Reviews component', () => {
+  it('Renders Reviews component',  () => {
     render(<Reviews />)
+    screen.debug()
   })
 })
