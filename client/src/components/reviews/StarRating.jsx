36c4568203ae627aa505e5c66f3792ab20269e65
Christian Willsea <christianwillsea@gmail.com>
2023-03-16 22:56:21 -0700

    Added a starRating component

---
+++ client/src/components/reviews/StarRating.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StarRating = ({ rating }) => {
+
+
+  return (
+    <div class="stars"> &#11088; &#11088; &#11088; &#11088; &#11088; </div>
+  )
+}
+
+export default StarRating;
