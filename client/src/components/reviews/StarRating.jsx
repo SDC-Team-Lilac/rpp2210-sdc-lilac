990ee5da56026f98e2f0363362b62c820fa404ec
Christian Willsea <christianwillsea@gmail.com>
2023-03-19 19:39:16 -0700

    Overlay empty stars with filled in stars

--- client/src/components/reviews/StarRating.jsx
+++ client/src/components/reviews/StarRating.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 const StarRating = ({ rating }) => {
 
+// console.log(starImage)
+
 
   return (
-    <div data-testid='starRating-1' className="reviews allStars">
-      <div className="reviews star"></div>
-      <div className="reviews star"></div>
-      <div className="reviews star"></div>
-      <div className="reviews star"></div>
-      <div className="reviews star"></div>
-
+    <div data-testid='starRating-1' className="reviews starRatings">
+      <span style={{width: '50%'}}className="fullStars" >
+        &#9733;&#9733;&#9733;&#9733;&#9733;
+      </span>
+      &#9734;&#9734;&#9734;&#9734;&#9734;
     </div>
   )
 }
+
+
+
+
 
 export default StarRating;
