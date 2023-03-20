0303550be10825c19be6b453c1e1c8986de272ea
Christian Willsea <christianwillsea@gmail.com>
2023-03-19 20:32:44 -0700

    Complete star rating component functionality

--- client/src/components/reviews/StarRating.jsx
+++ client/src/components/reviews/StarRating.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const StarRating = ({ rating }) => {
 
-// console.log(starImage)
+  const [starRating, setStarRating] = useState(0)
+  const [percentage, setPercentage] = useState('')
 
+  var percentageMap = {
+    25: 7,
+    50: 10,
+    75: 13,
+    1: 20,
+    2: 40,
+    3: 60,
+    4: 80,
+    5: 100
+  }
+
+  var quarterRating = (rating) => {
+    var result = (Math.round(Number(rating)  * 4) / 4).toFixed(2)
+    setStarRating(result)
+  }
+
+  var updatePercentage = (starRating) => {
+    var split = starRating.toString().split('.')
+    if (split[1] !== '00') {
+      var percentage = `${percentageMap[split[0]] + percentageMap[split[1]]}%`
+      setPercentage(percentage)
+    } else {
+      setPercentage(`${percentageMap[split[0]]}%`)
+    }
+  }
+
+  useEffect(() => {
+    quarterRating(rating)
+  }, [])
+
+  useEffect(() => {
+    updatePercentage(starRating)
+  }, [starRating])
 
   return (
     <div data-testid='starRating-1' className="reviews starRatings">
-      <span style={{width: '50%'}}className="fullStars" >
+      <span style={{width: percentage}}className="fullStars" >
         &#9733;&#9733;&#9733;&#9733;&#9733;
       </span>
       &#9734;&#9734;&#9734;&#9734;&#9734;
