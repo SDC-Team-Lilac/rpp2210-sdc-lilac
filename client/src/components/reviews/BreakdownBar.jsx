511bc9f08da601c4c06054e6e92ae611dffce68d
Christian Willsea <christianwillsea@gmail.com>
2023-03-27 17:47:35 -0700

    Added gray bar to productBreakdown

--- client/src/components/reviews/BreakdownBar.jsx
+++ client/src/components/reviews/BreakdownBar.jsx
@@ -10,7 +10,8 @@ const BreakdownBar = ({characteristic}) => {
   return (
     <div data-testid='breakdownBar-1' className="reviews breakdownBar">
       <div className="reviews breakdownBar characteristic">{characteristic}</div>
-      <div className="reviews breakdownBar bar">
+      <div className="reviews breakdownBar fullBar">
+        <div className="reviews breakdownBar bar"></div>
         <div>1 3 5</div>
         <div>▼</div>
       </div>
