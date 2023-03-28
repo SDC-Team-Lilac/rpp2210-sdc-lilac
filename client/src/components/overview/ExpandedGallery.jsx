92015d4f51772f5571740be4b5bf7e2d89b48d98
Rachel Quirbach <rachelquirbachy@gmail.com>
2023-03-28 11:45:07 -0400

    "Commit progress in adding mousemove functionality to zoom expanded mainImage - still a work in progress"

--- client/src/components/overview/ExpandedGallery.jsx
+++ client/src/components/overview/ExpandedGallery.jsx
@@ -2,14 +2,35 @@ import React, { useState } from 'react';
 
 const ExpandedGallery = ( { mainImage, imageDescription, setShowExpanded } ) => {
 
+  const [transformOrigin, setTransformOrigin] = useState({});
+  const [transform, setTransform] = useState('none');
+
   const handleCloseClick = (e) => {
     e.preventDefault();
     setShowExpanded(false);
   }
+
+
+  const handleExpandedMainImageClick = (e) => {
+    const expandedGallery = document.getElementById("expanded_gallery");
+    const expandedMainImage = document.querySelector("expanded_gallery_mainImage");
+    expandedGallery.addEventListener("mousemove", (e) => {
+      const x = e.clientX - e.target.offsetLeft;
+      const y = e.clientY - e.target.offsetTop;
+
+      // console.log(x, y);
+
+      setTransformOrigin(`${x}px ${y}px`);
+      setTransform('scale(2.5)');
+      // expandedMainImage.transformOrigin = `${x}px ${y}px`;
+      // expandedMainImage.transform = "scale(2.5)";
+    })
+  }
+
   return (
-    <div className="expanded_gallery">
+    <div id="expanded_gallery">
       {/* Hello, I'm the expanded gallery component! */}
-      <img className="expanded_gallery_mainImage" src={mainImage} alt={imageDescription}></img>
+      <img className="expanded_gallery_mainImage" src={mainImage} alt={imageDescription} transformOrigin={transformOrigin} transform={transform} onClick={handleExpandedMainImageClick}></img>
       <button className="expanded_gallery_close" onClick={handleCloseClick}>Close</button>
     </div>
   )
