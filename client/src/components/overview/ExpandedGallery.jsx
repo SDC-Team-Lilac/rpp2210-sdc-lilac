92b35797b6f90e555b1b8db9f81e64b03f60d9c3
Rachel Quirbach <rachelquirbachy@gmail.com>
2023-03-28 13:51:26 -0400

    "Commit progress on zooming image being responsive to mousehover in expanded view"

--- client/src/components/overview/ExpandedGallery.jsx
+++ client/src/components/overview/ExpandedGallery.jsx
@@ -2,35 +2,50 @@ import React, { useState } from 'react';
 
 const ExpandedGallery = ( { mainImage, imageDescription, setShowExpanded } ) => {
 
+  // Refactor need -- take care of right and bottom whitespace on zoomed image ***
+
+  const [isZoomed, setIsZoomed] = useState(false);
   const [transformOrigin, setTransformOrigin] = useState({});
   const [transform, setTransform] = useState('none');
+  const [cursor, setCursor] = useState('crosshair');
 
   const handleCloseClick = (e) => {
     e.preventDefault();
     setShowExpanded(false);
+    setIsZoomed(false);
   }
 
 
   const handleExpandedMainImageClick = (e) => {
-    const expandedGallery = document.getElementById("expanded_gallery");
-    const expandedMainImage = document.querySelector("expanded_gallery_mainImage");
-    expandedGallery.addEventListener("mousemove", (e) => {
+    console.log('Expanded Image Clicked!');
+    if (!isZoomed) {
+      setTransform('scale(2.5)');
+      // Refactor need -- make this a - sign instead ***
+      setCursor('zoom-out');
+      setIsZoomed(true);
+    } else {
+      setTransform('none');
+      setCursor('crosshair');
+      setIsZoomed(false);
+    }
+  }
+
+  const handleMouseMove = (e) => {
+    e.preventDefault();
+    if (isZoomed) {
       const x = e.clientX - e.target.offsetLeft;
       const y = e.clientY - e.target.offsetTop;
 
-      // console.log(x, y);
+      console.log(x, y);
 
       setTransformOrigin(`${x}px ${y}px`);
-      setTransform('scale(2.5)');
-      // expandedMainImage.transformOrigin = `${x}px ${y}px`;
-      // expandedMainImage.transform = "scale(2.5)";
-    })
+    }
   }
 
   return (
     <div id="expanded_gallery">
       {/* Hello, I'm the expanded gallery component! */}
-      <img className="expanded_gallery_mainImage" src={mainImage} alt={imageDescription} transformOrigin={transformOrigin} transform={transform} onClick={handleExpandedMainImageClick}></img>
+      <img className="expanded_gallery_mainImage" src={mainImage} alt={imageDescription} style={{transform: transform, transformOrigin: transformOrigin, cursor: cursor}} onMouseMove={handleMouseMove} onClick={handleExpandedMainImageClick}></img>
       <button className="expanded_gallery_close" onClick={handleCloseClick}>Close</button>
     </div>
   )
