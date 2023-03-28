5339749c812c13b8245cfff5f0f5e28157ffd944
Christian Willsea <christianwillsea@gmail.com>
2023-03-27 18:11:26 -0700

    Complete expanding width of gray bar

--- client/src/components/reviews/BreakdownBar.jsx
+++ client/src/components/reviews/BreakdownBar.jsx
@@ -11,9 +11,9 @@ const BreakdownBar = ({characteristic}) => {
     <div data-testid='breakdownBar-1' className="reviews breakdownBar">
       <div className="reviews breakdownBar characteristic">{characteristic}</div>
       <div className="reviews breakdownBar fullBar">
-        <div className="reviews breakdownBar bar"></div>
-        <div>1 3 5</div>
         <div>▼</div>
+        <div className="reviews breakdownBar charbar"></div>
+        <div>1 3 5</div>
       </div>
     </div>
   )
