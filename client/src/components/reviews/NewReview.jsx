bb26b63ad17d762c47f48687bd4c0618ec1104df
Christian Willsea <christianwillsea@gmail.com>
2023-03-28 19:33:16 -0700

    Add stars and recommend handlers

--- client/src/components/reviews/NewReview.jsx
+++ client/src/components/reviews/NewReview.jsx
@@ -12,7 +12,8 @@ const NewReview = ({reviewsMeta, onClose }) => {
       - product characteristics
    */
 
-  const [overall, setOverall] = useState('1')
+  const [overall, setOverall] = useState(null)
+  const [ratingDescription, setRatingDescription] = useState(null);
   const [recommend, setRecommend] = useState(false)
   const [characteristics, setCharacteristics] = useState({})
   const [summary, setSummary] = useState('Test Summary')
@@ -20,6 +21,7 @@ const NewReview = ({reviewsMeta, onClose }) => {
   const [photos, setPhotos] = useState([])
   const [nickname, setNickname] = useState('Nickname')
   const [email, setEmail] = useState('myemail@email.com')
+  const [starPercentage, setStarPercentage] = useState('0%')
 
 
 
@@ -45,47 +47,96 @@ const NewReview = ({reviewsMeta, onClose }) => {
     onClose();
   }
 
-
   const handleChange = (e) => {
     e.preventDefault();
     var input = e.target.value
   }
+
+  const handleStarClick = (e) => {
+    e.preventDefault();
+    let starName = e.target.attributes.name.nodeValue;
+    const starMapping = {
+      star1: {percent: '20%', description: "Poor"},
+      star2: {percent: '40%', description: "Fair"},
+      star3: {percent: '60%', description: "Average"},
+      star4: {percent: '80%', description: "Good"},
+      star5: {percent: '100%', description: "Great"}
+    }
+    setStarPercentage(starMapping[starName].percent)
+    setOverall(Number(starName.slice(-1)))
+    setRatingDescription(starMapping[starName].description)
+  }
+
+
+  const handleRecommendClick = (e) => {
+    e.preventDefault();
+    let option = e.target.value;
+    const recommendMap = {
+      Yes: true,
+      No: false
+    }
+    setRecommend(recommendMap[option])
+    console.log(option)
+  }
+
   return (
     <div data-testid='newReview-1' className="reviews newReview">
       <form onSubmit={handleSubmit}>
         Write your review about PRODUCT_NAME
-        <div>
-          <label>Overall Rating</label>
-          <input type="text"></input>
+        <div className="reviews newReviewItem overall">
+          <label >Overall Rating</label>
+          <div className="reviews newReviewItem stars">
+            <span style={{width: starPercentage}} className="newReviewStars" >
+            <span className="reviews newReviewItem fullStar1" name="star1" onClick={handleStarClick}>&#9733;</span>
+            <span className="reviews newReviewItem fullStar2" name="star2" onClick={handleStarClick}>&#9733;</span>
+            <span className="reviews newReviewItem fullStar3" name="star3" onClick={handleStarClick}>&#9733;</span>
+            <span className="reviews newReviewItem fullStar4" name="star4" onClick={handleStarClick}>&#9733;</span>
+            <span className="reviews newReviewItem fullStar5" name="star5" onClick={handleStarClick}>&#9733;</span>
+            </span>
+            <span className="reviews newReviewItem star1" name="star1" onClick={handleStarClick}>&#9734;</span>
+            <span className="reviews newReviewItem star2" name="star2" onClick={handleStarClick}>&#9734;</span>
+            <span className="reviews newReviewItem star3" name="star3" onClick={handleStarClick}>&#9734;</span>
+            <span className="reviews newReviewItem star4" name="star4" onClick={handleStarClick}>&#9734;</span>
+            <span className="reviews newReviewItem star5" name="star5" onClick={handleStarClick}>&#9734;</span>
+          </div>
+          {ratingDescription ? <div> {ratingDescription} </div>: null}
         </div>
-        <div>
+        <div className="reviews newReviewItem recommend">
           <label>Do you recommend this product?</label>
-          <input type="text"></input>
+            <label for="Yes">Yes</label>
+            <input type="radio" name="recommend" value="Yes" onClick={handleRecommendClick}></input>
+          <label for="No">No</label>
+            <input type="radio" name="recommend" value="No" onClick={handleRecommendClick}></input>
+
+          {/* <select className="reviews newReviewItem recommendOptions" onClick={handleRecommendClick}>
+            <option> Yes </option>
+            <option> No </option>
+          </select> */}
         </div>
-        <div>
+        <div className="reviews newReviewItem characteristic">
           <Characteristics characteristics={reviewsMeta.characteristics}/>
         </div>
-        <div>
+        <div className="reviews newReviewItem summary">
           <label>Review Summary</label>
           <input type="text"></input>
         </div>
-        <div>
+        <div className="reviews newReviewItem body">
           <label>Review Body</label>
           <input type="text"></input>
         </div>
-        <div>
+        <div className="reviews newReviewItem photos">
           <label>Upload Your Photos</label>
           <input type="text"></input>
         </div>
-        <div>
+        <div className="reviews newReviewItem nickname">
           <label>What is your nickname?</label>
           <input type="text"></input>
         </div>
-        <div>
+        <div className="reviews newReviewItem email">
           <label>What is your email?</label>
           <input type="text"></input>
         </div>
-        <div>
+        <div className="reviews newReviewItem submit">
           <label>Submit Review</label>
           <input type="submit"></input>
         </div>
