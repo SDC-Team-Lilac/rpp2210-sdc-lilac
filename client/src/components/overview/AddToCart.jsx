b3fe816802804905297a71f482b51bc2e2039442
Rachel Quirbach <rachelquirbachy@gmail.com>
2023-04-03 22:29:35 -0400

    "Commit refactored css for addToCart and Overview icons to not be as dark black"

--- client/src/components/overview/AddToCart.jsx
+++ client/src/components/overview/AddToCart.jsx
@@ -104,7 +104,7 @@ const AddToCart = ( { productDetails, selectedStyle, productStyles, myOutfit, se
       <div className="addToCart_bottom">
         {selectedQuantity === 0 ? null : <button data-testid="addToCartButton" className="addToCartButton" onClick={handleAddToCartClick}>Add to Cart</button>}
         <button data-testid="addToOutfitButton" className="addToOutfitButton" onClick={handleAddToOutfitClick}>
-          {inOutfit ? <img src="https://img.icons8.com/ios-filled/256/christmas-star.png" alt="In My Outfit" width="35px" height="35px"></img> : <img src="https://img.icons8.com/ios/256/christmas-star.png" alt="Add to Outfit" width="35px" height="35px"></img>}
+          {inOutfit ? <img className="addToOutfitIcon" src="https://img.icons8.com/ios-filled/256/christmas-star.png" alt="In My Outfit" width="35px" height="35px"></img> : <img className="addToOutfitIcon" src="https://img.icons8.com/ios/256/christmas-star.png" alt="Add to Outfit" width="35px" height="35px"></img>}
         </button>
       </div>
     </div>
