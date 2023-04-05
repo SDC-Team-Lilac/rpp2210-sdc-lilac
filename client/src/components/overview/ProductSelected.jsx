2f2106082ba4c75856a615c6ccc5175cb08731fa
Sarah <sarahkwando@gmail.com>
2023-04-05 00:45:56 -0500

    Fixed bug with add to outfit buttons!

--- client/src/components/overview/ProductSelected.jsx
+++ client/src/components/overview/ProductSelected.jsx
@@ -3,13 +3,13 @@ import ProductSummary from './ProductSummary.jsx';
 import StyleSelector from './StyleSelector.jsx';
 import AddToCart from './AddToCart.jsx';
 
-const ProductSelected = ( { productDetails, selectedStyle, productStyles, averageStarRating, totalNumberReviews, updateSelectedStyle, updateSelectedProduct, setMyOutfit, setOutfitCards, setProductId, inOutfit, setInOutfit, reviewsRef } ) => {
+const ProductSelected = ( { productDetails, selectedStyle, productStyles, averageStarRating, totalNumberReviews, updateSelectedStyle, updateSelectedProduct, myOutfit, setMyOutfit, setOutfitCards, setProductId, inOutfit, setInOutfit, reviewsRef } ) => {
 
   return (
     <div className="overview_product_selected">
       <ProductSummary productDetails={productDetails} selectedStyle={selectedStyle} averageStarRating={averageStarRating} totalNumberReviews={totalNumberReviews} reviewsRef={reviewsRef}/>
       <StyleSelector selectedStyle={selectedStyle} productStyles={productStyles} updateSelectedStyle={updateSelectedStyle} />
-      <AddToCart productDetails={productDetails} selectedStyle={selectedStyle} productStyles={productStyles} updateSelectedStyle={updateSelectedStyle} updateSelectedProduct={updateSelectedProduct} setMyOutfit={setMyOutfit} setOutfitCards={setOutfitCards} setProductId={setProductId} inOutfit={inOutfit} setInOutfit={setInOutfit} />
+      <AddToCart productDetails={productDetails} selectedStyle={selectedStyle} productStyles={productStyles} updateSelectedStyle={updateSelectedStyle} updateSelectedProduct={updateSelectedProduct} myOutfit={myOutfit} setMyOutfit={setMyOutfit} setOutfitCards={setOutfitCards} setProductId={setProductId} inOutfit={inOutfit} setInOutfit={setInOutfit} />
     </div>
   )
 }
