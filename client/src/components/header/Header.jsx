4526ff233a92fe3ee27d75ddd97495090edbebba
Rachel Quirbach <rachelquirbachy@gmail.com>
2023-04-07 16:06:19 -0400

    "Commit toggling of light/dark button icons and icon sizing/positioning"

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logoLight from './aerio_logo_light.png';
 
 const Header = () => {
 
+  const [lightMode, setLightMode] = useState(true);
+
+  const handleLightModeClick = (e) => {
+    e.preventDefault();
+    console.log('Light Mode Button Clicked!');
+    lightMode ? setLightMode(false) : setLightMode(true);
+  }
+
   return (
     <div id="header">
-      <img src={logoLight} height="35px"></img>
+      <img src={logoLight} alt="Atelier by Aerio (Logo)" height="35px"></img>
+      <button className="light_dark_mode_toggle">
+        {lightMode ? <img className="dark_mode_button" src="https://cdn-icons-png.flaticon.com/512/606/606807.png" alt="Change to Dark Mode" height="25px" onClick={handleLightModeClick}></img> : <img className="light_mode_button" src="https://cdn-icons-png.flaticon.com/512/702/702459.png" alt="Change to Light Mode" height="25px" onClick={handleLightModeClick}></img>}
+      </button>
     </div>
   );
 }
+
+// {mainImageIndex > 0 ? <img className="main_image_previous" onClick={handleMainPreviousClick} src="https://cdn-icons-png.flaticon.com/512/2732/2732652.png" alt="Previous"></img> : null}
 
 export default Header;
