ef7f8eea38fe26bc00534bf2a6c87df6cdd28d80
Christian Willsea <christianwillsea@gmail.com>
2023-05-06 12:52:41 -0700

    Complete setup connection with frontend

--- server/api_handlers/products.js
+++ server/api_handlers/products.js
@@ -3,41 +3,26 @@ require('dotenv').config();
 
 const APIHostURL = "https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp";
 const APIKey = process.env.FEC_API_KEY;
+const newAPIHostURL = process.env.HOST;
 
 // params:
   // page (INT) -- slects page of results to return, defaulted to 1
   // count (INT) -- specifies how many results per page to return, defaulted to 5
 const getAllProducts = () => {
-  return axios.get(`${APIHostURL}/products`, {
-    //optional key/values of page/pageNumber and count/countNumber
-    headers: {
-      Authorization: APIKey
-    }
-  });
+  return axios.get(`${newAPIHostURL}/products`);
 }
 
 const getOneProduct = (product_id) => {
-  return axios.get(`${APIHostURL}/products/${product_id}`, {
-    headers: {
-      Authorization: APIKey
-    }
-  })
+  console.log('---------------------------',product_id)
+  return axios.get(`${newAPIHostURL}/products/${product_id}`)
 }
 
 const getStyles = (product_id) => {
-  return axios.get(`${APIHostURL}/products/${product_id}/styles`, {
-    headers: {
-      Authorization: APIKey
-    }
-  })
+  return axios.get(`${newAPIHostURL}/products/${product_id}/styles`)
 }
 
 const getRelated = (product_id) => {
-  return axios.get(`${APIHostURL}/products/${product_id}/related`, {
-    headers: {
-      Authorization: APIKey
-    }
-  })
+  return axios.get(`${newAPIHostURL}/products/${product_id}/related`)
 }
 
 module.exports.getAllProducts = getAllProducts;
